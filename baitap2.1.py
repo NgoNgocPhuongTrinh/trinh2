df288fa14c25be0124d212e1a94485c1af73abdc
NgoNgocPhuongTrinh <trinhngo211@gmail.com>
2024-08-25 16:51:54 +0700

    Add files via upload

---
+++ baitap2.1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 08:04:32 2024
+
+@author: Student
+"""
+
+a = int(input("Nhap so thu nhat "))
+b = int(input("Nhap so thu hai "))
+c = int(input("Nhap so thu ba "))
+d = int(input("Nhap so thu tu "))
+e = (a+b+c+d)/4
+print(e)
